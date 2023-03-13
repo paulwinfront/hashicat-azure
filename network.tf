d70da76431bc010b625b22b58a55e65fb49515a4
paulwinfront <paulw@infront.net.au>
2023-03-13 02:04:23 +0000

    Updated network module

--- network.tf
+++ network.tf
@@ -1,15 +1,6 @@
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "example" {
-  name     = "gaurav-rg"
-  location = var.location
-}
-
 module "network" {
   source              = "Azure/network/azurerm"
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 
-  depends_on = [azurerm_resource_group.example]
+  depends_on = [azurerm_resource_group.myresourcegroup]
 }
